2b6c8cf539d5c3e3614114b326a00ee81d6052f0
shashi675 <shashikantrai675@gmail.com>
2026-05-25 23:58:20 +0530

    add logs

--- src/main/java/com/project/ecommerce/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/project/ecommerce/exceptionhandler/GlobalExceptionHandler.java
@@ -4,17 +4,19 @@ import com.project.ecommerce.dto.ApiResponse;
 import com.project.ecommerce.exception.CategoryAlreadyExitsException;
 import com.project.ecommerce.exception.ResourceNotFoundException;
 import com.project.ecommerce.exception.UserAlreadyExistsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ApiResponse<String>> handleUserNameNotFoundException(UserAlreadyExistsException e) {
-        e.printStackTrace();
+    public ResponseEntity<ApiResponse<String>> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(
                 new ApiResponse<>(false, e.getMessage(), null)
         );
@@ -22,7 +24,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<String>> handleResourceNotFoundException(ResourceNotFoundException e) {
-        e.printStackTrace();
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 new ApiResponse<>(false, e.getMessage(), null)
         );
@@ -30,7 +32,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CategoryAlreadyExitsException.class)
     public ResponseEntity<ApiResponse<String>> handleCategoryAlreadyExitsException(CategoryAlreadyExitsException e) {
-        e.printStackTrace();
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(
                 new ApiResponse<>(false, e.getMessage(), null)
         );
@@ -38,7 +40,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
-        e.printStackTrace();
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                 new ApiResponse(false, e.getMessage(), null)
         );
